a4bd29bf32fd6ed3c68713254237979b770f5093
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-14 17:54:32 +0100

    trying to make the games route appear

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home", { title: "Express" });
+  res.render("home");
+});
+
+router.get("/games", async (req, res, next) => {
+  try {
+    res.render("games", { games: await GameModel.find() });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
