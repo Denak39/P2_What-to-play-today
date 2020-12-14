7c046b85a30bf2411ed7ade6c18f8840f89d99ce
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-14 18:49:22 +0100

    still bugged

--- app.js
+++ app.js
@@ -11,30 +11,29 @@ require("./helpers/hbs"); // utils for hbs templates
 //   "Content-Type": "application/json",
 // };
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const settingsRouter = require("./routes/settings");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
+// hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -52,16 +51,17 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 //routes
-app.use("/", require("./routes/index"));
-app.use("/", require("./routes/users"));
+app.use("/", indexRouter);
+app.use("/", usersRouter);
 app.use("/", require("./routes/auth"));
+app.use("/", settingsRouter);
 
-app.get("/settings", (req, res) => {
-  res.render("settings", {
-    pageTitle: "settings",
-    layout: "layout2",
-  });
-});
+// app.get("/settings", (req, res) => {
+//   res.render("settings", {
+//     pageTitle: "settings",
+//     layout: "layout2",
+//   });
+// });
 
 // app.get("/recommend", (req, res) => {
 //   res.render("recommend", {
---
+++ routes/settings.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.render("settings");
+//   //     pageTitle: "settings",
+//   //     layout: "layout2",
+//   //   });
+// });
+module.exports = router;
