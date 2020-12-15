0f7e3dd826a23bb05d6ae9a6628cb1409b628057
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-15 16:47:44 +0100

    1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const GameModel = require("./../models/Game");
+const uploader = require("./../config/cloudinary");
+const protectPrivateRoute = require("./../middlewares/protectPrivateRoute");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -27,20 +29,38 @@ router.get("/game-add", (req, res) => {
   res.render("game_add");
 });
 
-router.post("/game-add", async (req, res, next) => {
-  const newGame = { ...req.body };
-  if (!req.file) {
-    newGame.image = undefined;
-  } else {
-    newGame.image = req.file.path;
+// router.post("/game-add", async (req, res, next) => {
+//   const newGame = { ...req.body };
+//   if (!req.file) {
+//     newGame.image = undefined;
+//   } else {
+//     newGame.image = req.file.path;
+//   }
+//   try {
+//     await GameModel.create(newGame);
+//     res.redirect("/games");
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.post(
+  "/game-add",
+  protectPrivateRoute,
+  uploader.single("image"),
+  async (req, res, next) => {
+    try {
+      //console.log("in try");
+      let newObj = req.body;
+      if (req.file) newObj.image = req.file.path;
+      let newGame = await GameModel.create(newObj);
+      console.log(`new product created, ${newGame}`);
+      res.redirect("./games");
+    } catch (err) {
+      next(err);
+    }
   }
-  try {
-    await GameModel.create(newGame);
-    res.redirect("/games");
-  } catch (err) {
-    next(err);
-  }
-});
+);
 
 router.get("/one-game/:id", async (req, res, next) => {
   GameModel.findById(req.params.id)
