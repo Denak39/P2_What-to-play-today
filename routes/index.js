091628f07ee1fdcb16d3a032a407407b247669c5
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-15 16:30:45 +0100

    0

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,29 @@ router.get("/games", async (req, res, next) => {
   }
 });
 
+router.get("/game-add", (req, res) => {
+  res.render("game_add");
+});
+
+router.post("/game-add", async (req, res, next) => {
+  const newGame = { ...req.body };
+  if (!req.file) {
+    newGame.image = undefined;
+  } else {
+    newGame.image = req.file.path;
+  }
+  try {
+    await GameModel.create(newGame);
+    res.redirect("/games");
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.get("/one-game/:id", async (req, res, next) => {
   GameModel.findById(req.params.id)
     .then((result) => res.render("one_game", { game: result }))
     .catch(next);
 });
-
-
 
 module.exports = router;
