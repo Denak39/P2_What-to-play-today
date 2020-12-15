ead28585b8f7df81a5e0249b7f00c5ee3feff618
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-15 11:58:39 +0100

    changed settings route

--- routes/settings.js
+++ routes/settings.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get("/settings", (req, res) => {
   res.render("settings", {
     pageTitle: "settings",
-    // layout: "layout2",
+    layout: "layout2",
   });
 });
 module.exports = router;
