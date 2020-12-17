fb3016afcb715d055f3f50627a2c39860f1c0cc9
Mathias Valverde <mathiasvalverde@hotmail.fr>
2020-12-17 12:19:06 +0100

    playlist add

--- routes/playlist.js
+++ routes/playlist.js
@@ -13,11 +13,27 @@ const GameModel = require("../models/Game");
 // });
 router.get("/playlist", protectAdminRoute, async (req, res, next) => {
   try {
-    res.render("playlist", { games: await LibraryModel.find() });
+    res.render("playlist", { users: await UserModel.find() });
   } catch (err) {
     next(err);
   }
 });
+
+router.get("/playlist-add", (req, res) => {
+  res.render("playlist_add");
+});
+
+router.post("/playlist-add", protectAdminRoute, async (req, res, next) => {
+  const newGame = { ...req.body };
+  console.log(newGame);
+  try {
+    await GameModel.create(newGame);
+    res.redirect("/games");
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.get("/userplaylist/:id", protectAdminRoute, async (req, res, next) => {
   // UserModel.findByIdAndUpdate
   LibraryModel.find(req.params.name)
